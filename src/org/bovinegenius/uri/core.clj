d57a09907fd782cda5e229d036f33ba4ce01b247
Walter Tetzner <walter@waltertetzner.net>
2011-01-28 22:23:39 -0500

    Initial Commit

---
+++ src/org/bovinegenius/uri/core.clj
@@ -0,0 +1,1 @@
+(ns org.bovinegenius.uri.core)
---
+++ test/org/bovinegenius/uri/test/core.clj
@@ -0,0 +1,6 @@
+(ns org.bovinegenius.uri.test.core
+  (:use [org.bovinegenius.uri.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
