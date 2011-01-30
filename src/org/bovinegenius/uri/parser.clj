9e4c729b61001e53350dde1d550dd0d970fc9a8c
Walter Tetzner <walter@waltertetzner.net>
2011-01-30 12:06:11 -0500

    Updated parsing functions to handle nil parameters in parser.clj.

--- src/org/bovinegenius/uri/parser.clj
+++ src/org/bovinegenius/uri/parser.clj
@@ -4,24 +4,39 @@
   "Takes a URI string and parses it into scheme, scheme-specific-part,
 and fragment parts."
   [uri]
-  (let [[_ scheme ssp fragment] (re-find #"^(.*?):([^#]+)#?(.*)$" uri)
-        fragment (if (.contains uri "#") fragment nil)]
-    {:scheme scheme :scheme-specific-part ssp :fragment fragment}))
+  (if uri
+    (let [[_ scheme ssp fragment] (re-find #"^(.*?):([^#]+)#?(.*)$" uri)
+          fragment (if (.contains uri "#") fragment nil)]
+      {:scheme scheme :scheme-specific-part ssp :fragment fragment})
+    {:scheme nil :scheme-specific-part nil :fragment nil}))
 
 (defn parse-scheme-specific
   "Parse the scheme specific part into authority, path, and query
 parts."
   [ssp]
-  (let [[_ authority path query] (re-find #"^//([^/]+)([^\?]+)\??(.*)$" ssp)
-        query (if (.contains ssp "?") query nil)]
-    {:authority authority :path path :query query}))
+  (if ssp
+    (let [[_ authority path query] (re-find #"^//([^/]+)([^\?]+)\??(.*)$" ssp)
+          query (if (.contains ssp "?") query nil)]
+      {:authority authority :path path :query query})
+    {:authority nil :path nil :query nil}))
 
 (defn parse-authority
   "Parse the authority part into user-info, hostname, and port parts."
   [authority]
-  (let [[_ user-info host port] (re-find #"^([^@]+(?=@))?@?([^:]+):?(.*)$" authority)
-        port (if (empty? port) nil (Integer/parseInt port))]
-    {:user-info user-info :host host :port port}))
+  (if authority
+    (let [[_ user-info host port] (re-find #"^([^@]+(?=@))?@?([^:]+):?(.*)$" authority)
+          port (if (empty? port) nil (Integer/parseInt port))]
+      {:user-info user-info :host host :port port})
+    {:user-info nil :host nil :port nil}))
+
+(defn clean-map
+  "Remove keys that have a nil value, and that are in the given set."
+  [data keys]
+  (reduce (fn [data key]
+            (if (data key)
+              data
+              (dissoc data key)))
+          data keys))
 
 (defn parse-uri
   "Parse a URI string."
