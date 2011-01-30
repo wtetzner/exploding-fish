9d3d5d243530a7d1e43471ed062d56833602eaa5
Walter Tetzner <walter@waltertetzner.net>
2011-01-30 12:05:23 -0500

    Added build-authority, build-scheme-specific, and build-uri-string to constructor.clj.

--- src/org/bovinegenius/uri/constructor.clj
+++ src/org/bovinegenius/uri/constructor.clj
@@ -1,3 +1,23 @@
 (ns org.bovinegenius.uri.constructor)
 
+(defn ^String build-authority
+  "Takes a map with :user-info, :host, and :port keys, and returns a
+string."
+  [{:keys [user-info host port]}]
+  (let [user-info (if user-info (format "%s@" user-info) nil)
+        port (if port (format ":%s" port) nil)]
+    (str user-info host port)))
 
+(defn ^String build-scheme-specific
+  "Takes a map with :authority, :path, and :query keys, and returns a
+string."
+  [{:keys [authority path query]}]
+  (let [query (if query (format "?%s" query) nil)]
+    (str "//" authority path query)))
+
+(defn ^String build-uri-string
+  "Takes a map representing a URI, and returns a string."
+  [{:keys [scheme scheme-specific-part fragment]}]
+  (let [scheme (if scheme (format "%s:" scheme) nil)
+        fragment (if fragment (format "#%s" fragment) nil)]
+    (str scheme scheme-specific-part fragment)))
