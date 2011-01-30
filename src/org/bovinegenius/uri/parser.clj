f53a7372fe8e39439d082665ede543b3fd568653
Walter Tetzner <walter@waltertetzner.net>
2011-01-30 01:52:27 -0500

    Added parse-scheme-specific, parse-authority, and parse-uri to parser.clj.

--- src/org/bovinegenius/uri/parser.clj
+++ src/org/bovinegenius/uri/parser.clj
@@ -7,3 +7,28 @@ and fragment parts."
   (let [[_ scheme ssp fragment] (re-find #"^(.*?):([^#]+)#?(.*)$" uri)
         fragment (if (.contains uri "#") fragment nil)]
     {:scheme scheme :scheme-specific-part ssp :fragment fragment}))
+
+(defn parse-scheme-specific
+  "Parse the scheme specific part into authority, path, and query
+parts."
+  [ssp]
+  (let [[_ authority path query] (re-find #"^//([^/]+)([^\?]+)\??(.*)$" ssp)
+        query (if (.contains ssp "?") query nil)]
+    {:authority authority :path path :query query}))
+
+(defn parse-authority
+  "Parse the authority part into user-info, hostname, and port parts."
+  [authority]
+  (let [[_ user-info host port] (re-find #"^([^@]+(?=@))?@?([^:]+):?(.*)$" authority)
+        port (if (empty? port) nil (Integer/parseInt port))]
+    {:user-info user-info :host host :port port}))
+
+(defn parse-uri
+  "Parse a URI string."
+  [uri]
+  (let [generic (parse-generic uri)
+        scheme-specific (parse-scheme-specific (:scheme-specific-part generic))
+        authority (parse-authority (:authority scheme-specific))]
+    (->> (merge generic scheme-specific authority)
+         (filter (fn [[key value]] (not (nil? value))))
+         (into {}))))
