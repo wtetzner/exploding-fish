26c7928dda82a13966edd2f25fec922d13968e32
Walter Tetzner <walter@waltertetzner.net>
2011-01-30 02:09:46 -0500

    Moved protocol back into uri.clj, and began implementing Uri.

--- src/org/bovinegenius/uri.clj
+++ src/org/bovinegenius/uri.clj
@@ -1,5 +1,5 @@
 (ns org.bovinegenius.uri
-  (:use (org.bovinegenius.uri query-string)
+  (:use (org.bovinegenius.uri query-string protocol parser constructor)
         (clojure.contrib def))
   (:import (java.net URI URL URLDecoder URLEncoder)))
 
@@ -12,6 +12,40 @@
   (if (string? *default-encoding*)
     *default-encoding*
     (deref *default-encoding*)))
+
+(defprotocol UniformResourceIdentifier
+  "Protocol for dealing with URIs."
+  (scheme
+    [self] [self new-scheme]
+    "Get or set the scheme component of the URI.")
+  (scheme-specific-part
+    [self] [self new-scheme-specific-part]
+    "Get or set the scheme specific part of the URI.")
+  (authority
+    [self] [self new-authority]
+    "Get or set the authority of the URI.")
+  (user-info
+    [self] [self new-user-info]
+    "Get or set the user-info of the URI.")
+  (host
+    [self] [self new-host]
+    "Get or set the host of the URI.")
+  (port
+    [self] [self new-port]
+    "Get or set the port of the URI.")
+  (path
+    [self] [self new-path]
+    "Get or set the path of the URI.")
+  (query
+    [self] [self new-query]
+    "Get or set the query string of the URI.")
+  (fragment
+    [self] [self new-fragment]
+    "Get or set the fragment of the URI."))
+
+(deftype Uri [data]
+  UniformResourceIdentifier
+  (scheme [self] (:scheme data)))
 
 (extend java.net.URI
   UniformResourceIdentifier
