35c0eb936f78c17c715f613d0fbed7b6272bec76
Walter Tetzner <walter@waltertetzner.net>
2011-01-30 01:28:15 -0500

    Added parse-scheme-specific to parser.clj.

--- src/org/bovinegenius/uri/parser.clj
+++ src/org/bovinegenius/uri/parser.clj
@@ -1,3 +1,9 @@
 (ns org.bovinegenius.uri.parser)
 
-
+(defn parse-scheme-specific
+  "Takes a URI string and parses it into scheme, scheme-specific-part,
+and fragment parts."
+  [uri]
+  (let [[_ scheme ssp fragment] (re-find #"^(.*?):([^#]+)#?(.*)$" uri)
+        fragment (if (.contains uri "#") fragment nil)]
+    {:scheme scheme :scheme-specific-part ssp :fragment fragment}))
