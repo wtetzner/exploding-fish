224a750c07968fe0eb7e07f54e8d2108032c5f10
Walter Tetzner <walter@waltertetzner.net>
2011-02-02 23:57:35 -0500

    Updated URI string building to handle URIs that only have a path component in constructor.clj.

--- src/org/bovinegenius/uri/constructor.clj
+++ src/org/bovinegenius/uri/constructor.clj
@@ -12,12 +12,17 @@ string."
   "Takes a map with :authority, :path, and :query keys, and returns a
 string."
   [{:keys [authority path query]}]
-  (let [query (if query (format "?%s" query) nil)]
-    (str "//" authority path query)))
+  (let [query (if query (format "?%s" query) nil)
+        authority (if (empty? authority) nil (format "//%s" authority))]
+    (str authority path query)))
 
 (defn ^String build-uri-string
   "Takes a map representing a URI, and returns a string."
-  [{:keys [scheme scheme-specific-part fragment]}]
+  [{:keys [scheme scheme-specific-part fragment] :as uri-data}]
   (let [scheme (if scheme (format "%s:" scheme) nil)
-        fragment (if fragment (format "#%s" fragment) nil)]
+        fragment (if fragment (format "#%s" fragment) nil)
+        authority (or (:authority uri-data) (build-authority uri-data))
+        scheme-specific-part (or scheme-specific-part
+                                 (build-scheme-specific
+                                  (assoc uri-data :authority authority)))]
     (str scheme scheme-specific-part fragment)))
