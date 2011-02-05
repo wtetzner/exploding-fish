246f81f6c74ff42507b4150b840148e984a8218b
Walter Tetzner <walter@waltertetzner.net>
2011-02-05 18:16:47 -0500

    Updated the description in project.clj.

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject org.bovinegenius/uri "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+  :description "URI library that defines a UniformResourceIdentifier
+  protocol, and implements it on a custom Uri class, as well as
+  java.net.URI, java.net.URL, and java.lang.String."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]])
