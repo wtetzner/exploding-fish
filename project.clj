2e95b073612fb80134846987141a99576a14a746
Walter Tetzner <walter@waltertetzner.net>
2012-05-04 21:03:33 -0400

    Added .lein-deps-sum to .gitignore.

--- project.clj
+++ project.clj
@@ -2,5 +2,4 @@
   :description "URI library that defines a UniformResourceIdentifier
   protocol, and implements it on a custom Uri class, as well as
   java.net.URI, java.net.URL, and java.lang.String."
-  :dependencies [[org.clojure/clojure "1.3.0"]]
-  :aot :all)
+  :dependencies [[org.clojure/clojure "1.3.0"]])
