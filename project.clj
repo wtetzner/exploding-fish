ca77d54b5a8bdec35cc1d4466fccf2992d4bb9d9
Walter Tetzner <walter@waltertetzner.net>
2012-05-06 14:03:41 -0400

    Release 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.bovinegenius/exploding-fish "0.1.1"
+(defproject org.bovinegenius/exploding-fish "0.2.0"
   :description "URI library that defines a UniformResourceIdentifier
   protocol, and implements it on a custom Uri class, as well as
   java.net.URI, java.net.URL, and java.lang.String."
