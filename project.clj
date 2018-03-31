5a33bba7d4e1c18080e64734600a16266c5a25af
Walter Tetzner <walter@waltertetzner.net>
2018-03-31 13:36:43 -0400

    Bumped to snapshot version.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject org.bovinegenius/exploding-fish "0.3.5"
+(defproject org.bovinegenius/exploding-fish "0.3.6-SNAPSHOT"
   :description "URI library that defines a UniformResourceIdentifier
   protocol, and implements it on a custom Uri class, as well as
   java.net.URI, java.net.URL, and java.lang.String."
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :deploy-repositories [["releases"  {:sign-releases false :url "https://clojars.org/repo"}]])
